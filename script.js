7e03920f4a6c6e9354cbed4fe2b56289abb440fb
Maxim Kaplin <kaplin95@inbox.ru>
2020-06-05 12:18:18 +0300

    add checking email

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ const showSuccess = (input) => {
     const formControl = input.parentElement
     formControl.className = 'form-control success'
 }
+// check email 
+const checkEmail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -27,8 +32,8 @@ form.addEventListener('submit', (e) => {
         showSuccess(username)
     }
 
-    if (email.value === '') {
-        showError(email, 'Email is required')
+    if (!checkEmail(email.value)) {
+        showError(email, 'Email is not valid')
     }
     else {
         showSuccess(email)

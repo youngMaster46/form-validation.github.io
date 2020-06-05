4742a629802024785aec5e78c7a1484638612894
Maxim Kaplin <kaplin95@inbox.ru>
2020-06-05 14:25:38 +0300

    make full validation for my form

--- script.js
+++ script.js
@@ -20,7 +20,12 @@ const showSuccess = (input) => {
 // check email 
 const checkEmail = (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(String(email.value).trim().toLowerCase())) {
+        showSuccess(email)
+    }
+    else {
+        showError(email, 'Email is incorrect')
+    }
 }
 const getFieldName = (input) => {
     const label = input.previousElementSibling
@@ -33,24 +38,40 @@ const checkRequired = (...rest) => {
             showError(el, `${getFieldName(el)} is required`)
         }
         else {
-            if (el.id.trim() === 'email') {
-                if (checkEmail(el.value)) {
-                    showSuccess(el)
-
-                    return el;
-                }
-                else {
-                    showError(el, 'Email is not correct')
-
-                    return el;
-                }
-            }
             showSuccess(el)
         }
     })
+}
+const checkLength = (element, min, max) => {
+    if (element.value.length < min) {
+        showError(element, `${getFieldName(element)} must be at least ${min} characters`)
+    }
+    else if (element.value.length > max) {
+        showError(element, `${getFieldName(element)} must be less than ${max}`)
+    }
+    else {
+        showSuccess(element)
+    }
+}
+const checkPasswordMatch = (input1, input2) => {
+    if (input1.value === input2.value) {
+        showSuccess(input1)
+        showSuccess(input2)
+        if (input1.value.length < 6) {
+            checkLength(input1, 6, 25)
+            checkLength(input2, 6, 25)
+        }
+    }
+    else {
+        showError(input2, `Passwords do not match`)
+    }
 }
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
     checkRequired(username, email, password, password2)
+    checkEmail(email)
+    checkLength(username, 3, 15)
+    checkLength(password, 6, 25)
+    checkPasswordMatch(password, password2)
 })
